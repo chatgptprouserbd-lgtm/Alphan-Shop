631f885dd98630bca392f0cfe7325962acb88357
chatgptprouserbd-lgtm <chatgptprouserbd@gmail.com>
2026-03-12 01:31:38 +0600

    Update database.py

--- database.py
+++ database.py
@@ -15,23 +15,7 @@ status TEXT
 )
 """)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS coupons(
-code TEXT,
-discount INTEGER
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS shop_items(
-id TEXT,
-name TEXT,
-price INTEGER
-)
-""")
-
 conn.commit()
-
 
 def add_order(order):
     cursor.execute(
@@ -40,36 +24,6 @@ def add_order(order):
     )
     conn.commit()
 
-
 def get_orders():
     cursor.execute("SELECT * FROM orders")
-    return cursor.fetchall()
-
-
-def add_coupon(code,discount):
-    cursor.execute(
-        "INSERT INTO coupons VALUES(?,?)",
-        (code,discount)
-    )
-    conn.commit()
-
-
-def get_coupon(code):
-    cursor.execute(
-        "SELECT * FROM coupons WHERE code=?",
-        (code,)
-    )
-    return cursor.fetchone()
-
-
-def add_item(i,name,price):
-    cursor.execute(
-        "INSERT INTO shop_items VALUES(?,?,?)",
-        (i,name,price)
-    )
-    conn.commit()
-
-
-def get_items():
-    cursor.execute("SELECT * FROM shop_items")
     return cursor.fetchall()
